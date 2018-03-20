e2bc1e3fe2459bace40962d9bab276bb269608ff
Brandon Leeds <brandonleeds@gmail.com>
2018-03-20 01:10:29 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
+class PostValidator < ActiveModel::Validator
+  def validate(record)
+    return false if record.title.to_s.empty?
+    return true unless record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top [number]") || record.title.include?("Guess")
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_with PostValidator
 end
